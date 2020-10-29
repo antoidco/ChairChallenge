8be8a7e3a6bf22d248f9b1886f124afd56a6e085
antoidco <antoidco@gmail.com>
2020-10-29 17:30:49 +0300

    Instantiate chairs and units at start

---
+++ Assets/Scripts/GameStartup.cs
@@ -0,0 +1,31 @@
+﻿using Com.Antoid.ChairChallenge;
+using UnityEngine;
+
+namespace com.Antoid.ChairChallenge {
+    public class GameStartup : MonoBehaviour {
+        public GameObject chairPrefab;
+        public GameObject unitPrefab;
+
+        private void Start() {
+            var gameData = FindObjectOfType<GameData>();
+            if (gameData == null) Debug.Log("No GameData in scene");
+
+            PrepareScene(gameData.chairCount, gameData.unitCount);
+        }
+
+        void Update() { }
+
+        private void PrepareScene(int chairs, int units) {
+            CreateObjectsInCircle(chairPrefab, chairs, 3);
+            CreateObjectsInCircle(unitPrefab, units, 8);
+        }
+
+        private void CreateObjectsInCircle(GameObject toCreate, int count, float radius) {
+            for (int i = 0; i < count; ++i) {
+                var angle = i * 2 * Mathf.PI / count ;
+                var relativePosition = new Vector3(radius * Mathf.Sin(angle), 0, radius * Mathf.Cos(angle));
+                Instantiate(toCreate, relativePosition, Quaternion.identity);
+            }
+        }
+    }
+}
