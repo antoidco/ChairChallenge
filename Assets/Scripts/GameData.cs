5aebe1fa884857ba5ee57301f1de951eeeb12260
antoidco <antoidco@gmail.com>
2020-10-29 17:04:52 +0300

    Initial commit: Added Launch scene with UI

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Com.Antoid.ChairChallenge {
+    public class GameData : MonoBehaviour {
+        public int chairCount;
+        public int unitCount;
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Com.Antoid.ChairChallenge {
+    public class Launcher : MonoBehaviour {
+        public InputField chairCountField;
+        public InputField unitCountField;
+
+        public const int ChairCountDefault = 6;
+        public const int UnitCountDefault = 10;
+
+        private bool _executed = false;
+        private void Start() {
+            chairCountField.text = ChairCountDefault.ToString();
+            unitCountField.text = UnitCountDefault.ToString();
+        }
+        public void OnGameLaunch() {
+            if (_executed) return;
+            _executed = true;
+
+            int chairCount = Convert.ToInt32(chairCountField.text);
+            int unitCount = Convert.ToInt32(unitCountField.text);
+
+            if (chairCount < 0) chairCount = ChairCountDefault;
+            if (unitCount < 0) unitCount = UnitCountDefault;
+
+            var gameDataObject = new GameObject("GameData");
+            DontDestroyOnLoad(gameDataObject);
+            var gameData = gameDataObject.AddComponent<GameData>();
+            gameData.chairCount = chairCount;
+            gameData.unitCount = unitCount;
+
+            SceneManager.LoadScene(1);
+        }
+    }
+}
